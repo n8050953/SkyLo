04bc3e2b9e7aa0a6cbc58302805ab6ee188c2687
tamcaogia <ngnh_mtam@yahoo.com>
2015-08-14 16:25:53 +1000

    Done script to clone identity database

--- SkyLo/Migrations/Configuration.cs
+++ SkyLo/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 namespace SkyLo.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SkyLo.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web;
 
     internal sealed class Configuration : DbMigrationsConfiguration<SkyLo.Models.ApplicationDbContext>
     {
@@ -15,6 +19,21 @@ namespace SkyLo.Migrations
 
         protected override void Seed(SkyLo.Models.ApplicationDbContext context)
         {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var roleManger = new RoleManager<ApplicationRole>(new RoleStore<ApplicationRole>(new ApplicationDbContext()));
+
+            manager.Create(new ApplicationUser() { UserName = "ngnh_mtam@yahoo.com", Email = "ngnh_mtam@yahoo.com" }, "T@m030192");
+            manager.Create(new ApplicationUser() { UserName = "ngnh_mtam@gmail.com", Email = "ngnh_mtam@gmail.com" }, "T@m030192");
+            manager.Create(new ApplicationUser() { UserName = "ngnh_mtam@bing.com", Email = "ngnh_mtam@bing.com" }, "T@m030192");
+            manager.Create(new ApplicationUser() { UserName = "ngnh_mtam@mail.com", Email = "ngnh_mtam@mail.com" }, "T@m030192");
+            manager.Create(new ApplicationUser() { UserName = "ngnh_mtam@yola.com", Email = "ngnh_mtam@yola.com" }, "T@m030192");
+
+            roleManger.Create(new ApplicationRole() { Name = "Staff", Description = "Bar staff" });
+            roleManger.Create(new ApplicationRole() { Name = "SuperAdmin", Description = "Application creator" });
+            roleManger.Create(new ApplicationRole() { Name = "Admin", Description = "System admin" });
+
+            manager.AddToRole(manager.FindByEmail("ngnh_mtam@yahoo.com").Id, "SuperAdmin");
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
